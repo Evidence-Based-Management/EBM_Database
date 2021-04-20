f37362808120f8348c9c0c707b85354c74e06c54
Daniel <raiga1234@gmail.com>
2021-04-19 19:35:07 -0500

    Add roles

---
+++ Tables/roles.sql
@@ -0,0 +1,10 @@
+-- Table: public.roles
+
+-- DROP TABLE public.roles;
+
+CREATE TABLE public.roles
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    "Name" character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT roles_pkey PRIMARY KEY (id)
+)
